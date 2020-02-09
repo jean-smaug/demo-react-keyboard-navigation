6c39d712ec81a683c8ebb732658be22fed79a388
maxime blanc <maximeblanc.dev@gmail.com>
2020-02-09 09:46:13 +0100

    it's wokring :)

--- src/App.js
+++ src/App.js
@@ -1,82 +1,94 @@
 import React from "react";
 
-const Context = React.createContext(null)
+const Context = React.createContext(null);
 
-const allowdedKeys = ["ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"]
+const allowdedKeys = ["ArrowUp", "ArrowDown"];
 
 function NavigationProvider(props) {
-  const [focusedItem, setFocusedItem] = React.useState("")
-  const [keys, setKeys] = React.useState([])
-
-  function registerKey(newKey) {
-    setKeys([...keys, newKey])
-  }
+  const [focusedItem, setFocusedItem] = React.useState(props.defaultFocus);
+  const [keys, setKeys] = React.useState([]);
 
   React.useEffect(() => {
     function handleKeyDown(e) {
-      if(!allowdedKeys.includes(e.key)) return
+      if (!allowdedKeys.includes(e.key)) return;
+      const eventName = e.key.substring(5, e.key.length).toLowerCase();
 
-      console.log(e.key)
+      const key = keys.find(key => key.name === focusedItem);
+
+      const target = key[eventName];
+      if (!!target) {
+        setFocusedItem(target);
+      }
     }
 
-    document.addEventListener("keydown", handleKeyDown)
+    document.addEventListener("keydown", handleKeyDown);
 
     return () => {
-      document.removeEventListener("keydown", handleKeyDown)
-    }
-  }, [])
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [keys, focusedItem]); // eslint-disable-line
 
-  return React.useMemo(() => (
-    <Context.Provider value={{ focus: focusedItem, setFocusedItem, registerKey }}>
+  function registerKey(newKey) {
+    setKeys(keys => [...keys, newKey]);
+  }
+
+  return (
+    <Context.Provider
+      value={{ focus: focusedItem, setFocusedItem, registerKey }}
+    >
       {props.children}
     </Context.Provider>
-  ), [])
-}
-
-function withNavigation(Component) {
-  return function (props) {
-    return (
-      <Context.Consumer>
-        {
-          context => {
-            function capitalize(str) {
-              return str[0].toUpperCase().concat(str.slice(1, str.length))
-            }
-
-            const name = props.name
-            const handlers = Object.keys(props).filter(prop => allowdedKeys.includes(`Arrow${capitalize(prop)}`))
-            context.registerKey({ name })
-            // console.log(context)
-            // console.log(handlers)
-            // console.log(name)
-
-            return <Component {...props} {...context} />
-          }
-        }
-      </Context.Consumer>
-    )
-  }
+  );
 }
 
 function Child(props) {
-  return <div>{props.children}</div>
-}
+  const ctx = React.useContext(Context);
+  const [focus, setFocus] = React.useState(false);
 
-const Element1 = withNavigation(Child)
-const Element2 = withNavigation(Child)
+  const { name, up, down } = props;
+  React.useEffect(() => {
+    ctx.registerKey({ name, up, down });
+  }, []); // eslint-disable-line
+
+  React.useEffect(() => {
+    if (ctx.focus === props.name) {
+      setFocus(focus => !focus);
+    }
+    if (focus && ctx.focus !== props.name) {
+      setFocus(focus => !focus);
+    }
+  }, [ctx.focus]); // eslint-disable-line
+
+  return React.useMemo(() => {
+    console.log("useMemo", props.name);
+    return (
+      <div style={{ color: focus ? "blue" : "red" }}>{props.children}</div>
+    );
+  }, [focus]); // eslint-disable-line
+}
 
 function App() {
   return (
-    <NavigationProvider>
-      <Element1 name="one" down="two">
-        Element 1
-      </Element1>
-      
-      <Element2 name="two" up="one">
-        Element 2
-      </Element2>
-    </NavigationProvider>
-  )
+    <div style={{ margin: "20px" }}>
+      <NavigationProvider defaultFocus="one">
+        <Child name="one" down="two">
+          Element 1
+        </Child>
+
+        <Child name="two" up="one" down="third">
+          Element 2
+        </Child>
+
+        <Child name="third" up="two" down="fourth">
+          Element 3
+        </Child>
+
+        <Child name="fourth" up="third">
+          Element 4
+        </Child>
+      </NavigationProvider>
+    </div>
+  );
 }
 
 export default App;
